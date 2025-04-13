e2325fad71e51aa5bcc09030b3e870ac247e1e53
Filip <archaikek>
2025-04-13 15:22:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace WebCrawler
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please provide a starting URL.");
+			var origin = Console.ReadLine();
+
+			var web = new HtmlWeb();
+			var document = web.Load(origin);
+			
+			Console.WriteLine("Page loaded successfully");
+		}
+	}
+}
