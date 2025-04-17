8911e09e49b99c10299af2aa65b121b3631ba44f
Filip <archaikek>
2025-04-17 15:19:32 +0200

    Further finetuning of link selection (proper handling of links ending with '.html', '.pdf' and neither of those, and links starting with '/' instead of the domain origin. Fixed problems with running all threads and errors in saving pdfs.

--- Crawler.cs
+++ Crawler.cs
@@ -7,13 +7,15 @@ using System.Collections.Concurrent;
 using HtmlAgilityPack;
 using TurnerSoftware.RobotsExclusionTools;
 using System.Security;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace web_crawler
 {
 	internal class Crawler
 	{
 		private static LockedInt foundCount = new LockedInt(1);
-		public static readonly int limit = 444;
+		public static readonly int limit = 4444;
 
 		public static Graph graph { get; set; } = null;
 		public static ConcurrentQueue<KeyValuePair<string, int>> urlQueue { get; } = new ConcurrentQueue<KeyValuePair<string, int>>();
@@ -21,6 +23,9 @@ namespace web_crawler
 		public static string origin { private get; set; } = String.Empty;
 		public static RobotsFile robotsFile { private get; set; } = null;
 		public static HtmlWeb web { private get; set; } = null;
+
+		private static int pdfSkipCounter = 0;
+		private static readonly int pdfSkipLimit = 44;
 		public Crawler() 
 		{
 			if (graph is null)
@@ -36,6 +41,7 @@ namespace web_crawler
 			{
 				CrawlNext();
 			}
+			Console.WriteLine($"Thread finished. Total found: {foundCount.value}\n");
 		}
 
 		public void CrawlNext()
@@ -52,21 +58,45 @@ namespace web_crawler
 			var currentDocument = web.TryLoad(currentUrl.Key);
 			var links = currentDocument.DocumentNode.SelectNodes("//a[@href]");
 
-			Task saver = Task.Run(() => {
-				using (FileStream fs = File.OpenWrite(MakeFileName(currentUrl.Value, currentUrl.Key)))
+			Thread saver = new Thread(() => {
+				if (currentUrl.Key.Substring(currentUrl.Key.Length - 4) == ".pdf")
 				{
-					currentDocument.Save(fs);
+					try
+					{
+						using (var client = new WebClient()) client.DownloadFile(currentUrl.Key, MakeFileName(currentUrl.Value, currentUrl.Key));
+					} 
+					catch { }
+				}
+				else
+				{
+					using (FileStream fs = File.OpenWrite(MakeFileName(currentUrl.Value, currentUrl.Key)))
+					{
+						currentDocument.Save(fs);
+					}
 				}
 			});
+			saver.Start();
 
 			if (links is null) return;
 
 			foreach (var link in links)
 			{
 				var url = link.GetAttributeValue("href", String.Empty);
-				int index = url.IndexOf('?');
-				if (index != -1) url = url.Substring(0, url.IndexOf('?'));
+				if (url.StartsWith('/')) url = origin + url.Substring(1);
 				if (!url.StartsWith(origin)) continue;
+				if (Regex.Matches(url, "pdf").Count > 1) continue; // save some more interesting pdfs, but generally skip those in major pdf dump folders
+				if (Regex.Matches(url, "pdf").Count == 1) // also allow only 1 in pdfSkipLimit pdfs (apparently there are way too many of them)
+				{
+					++pdfSkipCounter; // no lock here because the precision isn't that important here
+					if (pdfSkipCounter >= pdfSkipLimit) pdfSkipCounter = 0;
+					else continue;
+				}
+
+				int index = url.IndexOf('?');
+				if (index != -1) url = url.Substring(0, index);
+				index = url.IndexOf('#');
+				if (index != -1) url = url.Substring(0, index);
+
 				if (!robotsFile.IsAllowedAccess(new Uri(url), "TestCrawer2221")) continue;
 
 				lock (foundCount)
@@ -88,12 +118,15 @@ namespace web_crawler
 				}
 			}
 
-			saver.Wait();
+			saver.Join();
+			Console.WriteLine($"Finished crawling page {currentUrl.Value}");
 		}
 
 		private string MakeFileName(int index, string currentUrl)
 		{
-			return Program.pagesPath + $"{index}_{currentUrl.Replace(':', '-').Replace('/', '_').Replace('\\', '_')}.html";
+			string suffix = "";
+			if (!(currentUrl.Substring(currentUrl.Length - 5, 5) == ".html") && !(currentUrl.Substring(currentUrl.Length - 4, 4) == ".pdf")) suffix = ".html";
+			return Program.pagesPath + $"{index}_{currentUrl.Replace(':', '-').Replace('/', '_').Replace('\\', '_')}{suffix}";
 		}
 
 		internal class LockedInt
--- Program.cs
+++ Program.cs
@@ -11,20 +11,31 @@ namespace web_crawler
 	{
 		public static string pagesPath = "../../../pages/";
 		public static string graphFilename = "graph.in";
+		public static string timeFilename = "times.txt";
 		static void Main(string[] args)
 		{
-			int threadCount = Int32.Parse(args[0]);
+			if (!Directory.Exists(pagesPath))
+			{
+				Directory.CreateDirectory(pagesPath);
+			}
+
+			int threadCount = 1;
+			try { threadCount = Int32.Parse(args[0]); } catch { threadCount = 12; }
 			Console.WriteLine($"Running with {threadCount} threads.");
 
 			Console.WriteLine("Please provide a starting URL.");
-			var origin = "https://pg.edu.pl";
+			//var origin = "https://pg.edu.pl/";
+			var origin = "https://www.tus.ac.jp/";
 			//var origin = Console.ReadLine();
+
+			var watch = System.Diagnostics.Stopwatch.StartNew();
 
 			var robotsFileParser = new RobotsFileParser();
 			Task<RobotsFile> robotsFileTask = robotsFileParser.FromUriAsync(new Uri($"{origin}/robots.txt"));
 			var robotsFile = robotsFileTask.Result;
 
 			var web = new HtmlWeb();
+			web.OverrideEncoding = System.Text.Encoding.UTF8;
 			var document = web.TryLoad(origin);
 
 			Crawler.origin = origin;
@@ -33,28 +44,29 @@ namespace web_crawler
 
 			Crawler.urlQueue.Enqueue(new KeyValuePair<string, int>(origin, 0));
 
-			// run this once so that there are actual links in the queue
+			// run this a few times so that there are actual links in the queue
 			Crawler[] crawlers = new Crawler[threadCount];
 			crawlers[0] = new Crawler();
-			crawlers[0].CrawlNext();
+			while (Crawler.urlQueue.Count < threadCount * 2 && Crawler.urlQueue.Count > 0) crawlers[0].CrawlNext();
 
 			//threadCount = 1; // uncomment this to simulate a single-threaded crawler
 			Task[] tasks = new Task[threadCount - 1];
 			for (int i = 1; i < threadCount; ++i)
 			{
 				var crawler = crawlers[i] = new Crawler();
-				tasks[i - 1] = Task.Run(() => crawler.CrawlLoop());
+				tasks[i - 1] = Task.Factory.StartNew(() =>  crawler.CrawlLoop(), TaskCreationOptions.LongRunning);
 			}
 			crawlers[0].CrawlLoop();
+
 			Task.WaitAll(tasks);
 
-			//Console.WriteLine(Crawler.graph);
+			watch.Stop();
+			var timeElapsed = watch.ElapsedMilliseconds;
 
-			if (!Directory.Exists(pagesPath))
-			{
-				Directory.CreateDirectory(pagesPath);
-			}
+			//Console.WriteLine(Crawler.graph);
 			File.WriteAllText(pagesPath + graphFilename, $"{Crawler.graph.Nodes.Count}\n" + Crawler.graph.ToString());
+
+			File.AppendAllText(pagesPath + timeFilename, $"{DateTime.Now}: Gathered {Crawler.graph.Nodes.Count} pages in {timeElapsed} ms using {threadCount} threads.\n");
 		}
 	}
 }
